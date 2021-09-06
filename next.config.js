be3bf5bf36d161f437c29ba4eae30be22acda448
Ismael Rumzan <ismael.rumzan@gmail.com>
2021-09-06 13:06:37 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'my custom header value',
+          },
+          {
+            key: 'x-another-custom-header',
+            value: 'my other custom header value',
+          },
+        ],
+      },
+    ]
+  },  
 }
